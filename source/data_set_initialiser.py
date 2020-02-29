5511047b44d8a6e84e0fffc48ea7716fec058a22
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-02-29 21:01:16 +1300

    Create data_set_initialiser.py

---
+++ source/data_set_initialiser.py
@@ -0,0 +1,22 @@
+"""
+Take a data set of images and assign a colour to each of these images based on average colour in image
+and save it as a dictionary or something, mapping colour value to img name.
+
+Tasks:
+    -   Iterate over directory and load each image into memory then get rid of it, should return list of names
+    -   When image is loaded in it avg the colour values of each pixel and map it to the name
+
+@author Jakob McKinney
+"""
+import sys
+import cv2
+
+
+def init_data_set(dir_name):
+    """
+
+    """
+
+
+if __name__ == "__main__":
+    init_data_set(sys.argv[1])
