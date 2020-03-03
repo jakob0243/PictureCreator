1d799409d1489114734bb729e6d5ae26dbe7c418
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-03-03 17:50:41 +1300

    Read, crop and save image

--- source/data_set_initialiser.py
+++ source/data_set_initialiser.py
@@ -10,13 +10,38 @@ Tasks:
 """
 import sys
 import cv2
+import numpy
 
 
-def init_data_set(dir_name):
+def init_data_set():
     """
-
+    Reads image, splits it up into individual images and saves them
     """
+    img = cv2.imread("./DataSets/dataset_pokemon.png")
+    cv2.imshow("Image", img)
+
+    print(img.shape)
+
+    x = 0
+    y = 160
+    for row in range(0, 26):
+        print(f"Start: x, y")
+        img = cv2.imread("./DataSets/dataset_pokemon.png")
+        for col in range(0, 18):
+            cropped_img = img.copy()
+            cropped_img = cropped_img[x:x+80, y:y+80]
+            cv2.imshow("Cropped Img", cropped_img)
+            cv2.imwrite(f"./DataSets/Images/{row}-{col}.png", cropped_img)
+            x += 80
+            print(x, y)
+        x = 0
+        y += 80
+        print(y)
+
+    cv2.destroyAllWindows()
+
+
 
 
 if __name__ == "__main__":
-    init_data_set(sys.argv[1])
+    init_data_set()
