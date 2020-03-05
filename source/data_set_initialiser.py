50f926452817e0dcff55cd7e29480d7f94739a6a
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-03-05 22:41:00 +1300

    average image colours

--- source/data_set_initialiser.py
+++ source/data_set_initialiser.py
@@ -10,7 +10,51 @@ Tasks:
 """
 import sys
 import cv2
-import numpy
+import numpy as np
+
+
+IMAGE_DIR = "./DataSets/Images/"
+IMAGE_HEIGHT = 80
+IMAGE_WIDTH = 80
+
+
+def avg_colour(img):
+    """
+    Averages the colour of all the pixels in the image.
+    Ignores black backgrounds.
+    Could average r values then g values then b values and return an actual colour
+
+    @param img an image
+    @return the average value of the pixels
+    """
+    print(len(img))
+    print(img[40])
+    total = 0
+    total2 = 0
+    total3 = 0
+    num_pixels = 0
+    for row in img:
+        for pixel in row:
+            p_sum = sum(pixel)
+            # Ignore if it is a black pixel as this is the background colour
+            if p_sum > 0:
+                total += pixel[0]
+                total2 += pixel[1]
+                total3 += pixel[2]
+                num_pixels += 1
+                print(p_sum)
+
+    print(f"Total: {total}")
+    print(f"No. pixels: {num_pixels}")
+    return [(total / num_pixels), (total2 / num_pixels), (total3 / num_pixels)]
+
+
+def read_images():
+    """
+    Read images in directory, average the colour for each
+    image and save it.
+    """
+    pass
 
 
 def init_data_set():
@@ -23,25 +67,30 @@ def init_data_set():
     print(img.shape)
 
     x = 0
-    y = 160
+    y = 0
     for row in range(0, 26):
-        print(f"Start: x, y")
         img = cv2.imread("./DataSets/dataset_pokemon.png")
         for col in range(0, 18):
             cropped_img = img.copy()
-            cropped_img = cropped_img[x:x+80, y:y+80]
-            cv2.imshow("Cropped Img", cropped_img)
+            cropped_img = cropped_img[x:x+IMAGE_WIDTH, y:y+IMAGE_HEIGHT]
             cv2.imwrite(f"./DataSets/Images/{row}-{col}.png", cropped_img)
-            x += 80
+            x += IMAGE_WIDTH
             print(x, y)
         x = 0
-        y += 80
-        print(y)
+        y += IMAGE_HEIGHT
 
     cv2.destroyAllWindows()
 
 
-
-
 if __name__ == "__main__":
-    init_data_set()
+    #  init_data_set()
+    image = cv2.imread(IMAGE_DIR + '1-9.png')
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    colour = avg_colour(image)
+    print(colour)
+    # NEEDS TO BE <=255
+    img2 = [[colour for x in range(0, 80)] for x in range(0, 80)]
+    img2 = np.array(img2)
+    cv2.imshow('image', img2)
+    cv2.waitKey(0)
