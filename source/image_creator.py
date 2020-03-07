a57e80f0ad28a5197f5e8eb6e3354de829256043
jakob0243 <48614646+jakob0243@users.noreply.github.com>
2020-03-07 21:42:07 +1300

    Update image_creator.py

--- source/image_creator.py
+++ source/image_creator.py
@@ -23,6 +23,16 @@ def load_data():
 IMAGE_DICT = load_data()
 BACKGROUND_COLOUR = "black"
 BACKGROUND_IMAGE_PATH = "./DataSets/Images/24-17.png"
+IMAGES_USED = {}
+
+
+def rotate_image(image, angle):
+    row, col, _ = image.shape
+    center = tuple(np.array([row, col])/2)
+    rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
+    new_image = cv2.warpAffine(image, rot_mat, (col, row))
+
+    return new_image
 
 
 def find_nearest_colour(bgr_colour):
@@ -43,14 +53,47 @@ def find_nearest_colour(bgr_colour):
         # dif vector of blue components ** 2, + dif vector of green components ** 2, + dif vector of red components ** 2
         # Each colour has vector has a box where the diagonal is the vector and the sides are the r, g  and b components
         # below calculates the difference between these 2 boxes using 3-d pythagoras.
-        d = math.sqrt(((colour[0]-bgr_colour[0])*0.3) ** 2
+        """d = math.sqrt(((colour[0]-bgr_colour[0])*0.3) ** 2
                       + ((colour[1]-bgr_colour[1])*0.59) ** 2
-                      + ((colour[2]-bgr_colour[2])*0.11) ** 2)
+                      + ((colour[2]-bgr_colour[2])*0.11) ** 2)"""
+        d = math.sqrt((colour[0] - bgr_colour[0]) ** 2
+                      + (colour[1] - bgr_colour[1]) ** 2
+                      + (colour[2] - bgr_colour[2]) ** 2)
         if d < min_d:
             min_d = d
             min_colour_key = name
 
     return min_colour_key
+
+
+def add_to_image(img_array, new_image_name, column, row):
+    """
+    Add pixel_img to the img array.
+
+    @param img_array the image being created
+    @param new_image_name name of the image to be added
+    @param column the current column in the original image
+    @param row the current row in the original image
+    @return the new image with the image created
+    """
+    if new_image_name not in IMAGES_USED.keys():
+        pixel_img = cv2.imread(f"./DataSets/Images/{new_image_name}")
+        IMAGES_USED[new_image_name] = pixel_img.copy()
+    else:
+        pixel_img = IMAGES_USED[new_image_name].copy()
+
+
+    row_nu = row * 80
+    col_nu = column * 80
+    for pixel_row in pixel_img:
+        for pix in pixel_row:
+            if sum(pix) != 0:
+                img_array[row_nu][col_nu] = pix
+            col_nu += 1
+        col_nu = column * 80
+        row_nu += 1
+
+    return img_array
 
 
 def create_img(img_path):
@@ -64,7 +107,7 @@ def create_img(img_path):
     """
     new_image = np.zeros((6400, 6400, 3), np.uint8)
     img = cv2.imread(img_path)
-    cv2.imshow("Diagla", img)
+    cv2.imshow(img_path[18:], img)
     cv2.waitKey(0)
 
     current_row = 0
@@ -73,22 +116,14 @@ def create_img(img_path):
         for pixel in row:  # Iterate over each pixel in that row
             picture_for_pixel = find_nearest_colour(pixel)
             if picture_for_pixel != BACKGROUND_COLOUR:
-                pixel_img = cv2.imread(f"./DataSets/Images/{picture_for_pixel}")
-                row_num = current_row * 80
-                col_num = current_col * 80
-                for pixel_row in pixel_img:
-                    for pix in pixel_row:
-                        if sum(pix) != 0:
-                            new_image[row_num][col_num] = pix
-                        col_num += 1
-                    col_num = current_col * 80
-                    row_num += 1
+                new_image = add_to_image(new_image, picture_for_pixel, current_row, current_col)
             current_col += 1
         current_col = 0
         current_row += 1
         print(f"Working on row: {current_row}...")
         print(f"Row {current_row} completed!")
-    cv2.imwrite(f"./DataSets/Finished/{img_path[18:]}", new_image)
+
+    cv2.imwrite(f"./DataSets/Finished/Test{img_path[18:]}", rotate_image(new_image, 270))  # {img_path[18:]}", new_image) flip 0
     print("Image completed!")
 
 
@@ -98,11 +133,19 @@ if __name__ == "__main__":
 
     # create_img("./DataSets/Images/6-17.png")
     # create_test()
-    start = time.time()
-    create_img("./DataSets/Images/0-0.png")
+    img_path = "./DataSets/Images/2-14.png"
+    """start = time.time()
+    create_img("./DataSets/Images/2-14.png")
     end = time.time()
     print(f"{end - start} seconds to execute")
-    """
+    """"""
     Time before optimisations: 115.4655818939209 seconds to execute on raquaza
-    Time after optimisations:
+    Time after adding IMAGES_USED dict: 112.70291018486023
+    
+    TODO: make sure it is made correctly so don't need to flip and pics will be correct orientation
     """
+    # cv2.imwrite("./DataSets/Finished/Test.png", cv2.flip(read, 0))
+    # read = cv2.imread("./DataSets/Finished/Test.png")
+    # cv2.imwrite("./DataSets/Finished/Test.png", rotate_image(read, 270))
+    read = cv2.imread(f"./DataSets/Finished/Test{img_path[18:]}")
+    cv2.imwrite(f"./DataSets/Finished/Test{img_path[18:]}", cv2.flip(read, 1))
